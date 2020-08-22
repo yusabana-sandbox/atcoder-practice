4dc2189e479a4c90fa0716ff7ca8c42b5058f259
Yuji Takaesu <taka.esu.user@gmail.com>
2020-08-22 23:53:15 +0900

    feat: ABC175 by refer from answer

--- ABC/ABC175/golang/a.go
+++ ABC/ABC175/golang/a.go
@@ -11,11 +11,33 @@ func RunA() {
 
 	//fmt.Print(strings.Count(s, "R"))
 
-	if s == "RRR" {
+	if strings.Contains(s, "RRR") {
 		fmt.Println("3")
 	} else if strings.Contains(s, "RR") {
 		fmt.Println("2")
 	} else if strings.Contains(s, "R") {
+		fmt.Println("1")
+	} else {
+		fmt.Println("0")
+	}
+}
+
+func RunAAnswer() {
+	var s1 string
+	var s2 string
+	var s3 string
+
+	fmt.Scanf("%s%s%s", &s1, &s2, &s3)
+
+	b1 := s1 == "R"
+	b2 := s2 == "R"
+	b3 := s3 == "R"
+
+	if b1 && b2 && b3 {
+		fmt.Println("3")
+	} else if (b1 && b2) || (b2 && b3) {
+		fmt.Println("2")
+	} else if b1 || b2 || b3 {
 		fmt.Println("1")
 	} else {
 		fmt.Println("0")
--- ABC/ABC175/golang/main.go
+++ ABC/ABC175/golang/main.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("Run...")
 
 	RunA()
+	//RunAAnswer()
 }
