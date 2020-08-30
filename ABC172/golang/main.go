78dd8a64ac94c4811ea63f45eb06a81303bcf00a
Yuji Takaesu <taka.esu.user@gmail.com>
2020-08-30 22:57:09 +0900

    feat: Remove unnecessary codes from ABC172 at golang

--- ABC172/golang/main.go
+++ ABC172/golang/main.go
@@ -12,12 +12,5 @@ func main() {
 func a() {
 	var n float64
 	fmt.Scan(&n)
-
-	if n == 'a' { }
-
-
-	msg := "world"
-	fmt.Printf("hello, %s!", msg)
-
 	fmt.Println(n + math.Pow(n, 2)+ math.Pow(n, 3))
 }
