b8d2257c16cbceb924e882c152a9c009b7dd68db
Yuji Takaesu <taka.esu.user@gmail.com>
2020-09-04 11:32:37 +0900

    feat: use slice for function of extract paticular value

--- ABC170/golang/main.go
+++ ABC170/golang/main.go
@@ -9,12 +9,17 @@ func main() {
 }
 
 func a() {
-	var list [5]int
+	// makeでスライスを作ってpositionを求めるクロージャもスライスを受け取れるようにする
+	// varでlistを定義するだけだと長さが指定できないのでScanで &list[0] のようなアクセスでエラーが出る
+	//var list []int
+	list := make([]int, 5, 10)
 
-	fmt.Scan(&list[0], &list[1], &list[2], &list[3], &list[4]) // スペース区切りなのでそのままScanで取れる
+	// スペース区切りなのでそのままScanで取れる
+	fmt.Scan(&list[0], &list[1], &list[2], &list[3], &list[4])
 
 	// 0を代入したポジションを返す
-	pos := func(arr [5]int, e int) int {
+	// https://gawawa124.hatenablog.com/entry/2015/04/08/193237
+	pos := func(arr []int, e int) int {
 		for i, v := range arr {
 			if v == e { return i + 1 }
 		}
