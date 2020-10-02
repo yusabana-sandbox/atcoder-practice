9307e44eadde86ce70be1b31960e5d589aad59e8
Yuji Takaesu <taka.esu.user@gmail.com>
2020-10-03 08:15:20 +0900

    feat: Answer Question A of ABC179 by golang

--- ABC179/golang/main.go
+++ ABC179/golang/main.go
@@ -4,10 +4,21 @@ import "fmt"
 
 func main() {
 	a()
-	
 }
 
 func a() {
-	fmt.Println("AAA")
-}
+	// https://www.it-swarm-ja.tech/ja/string/golang文字列の最後のx文字を取得する方法は？/1049363837/
+	var str string
+	fmt.Scan(&str)
 
+	// indexを指定して取得パターン
+	//fmt.Println(string(str[len(str)-1]))
+	// 範囲指定を使ってで取得
+	last := str[len(str)-1:]
+
+	if last == "s" {
+		fmt.Println(str + "es")
+	} else {
+		fmt.Println(str + "s")
+	}
+}
