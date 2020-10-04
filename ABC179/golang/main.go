466466ca5cce3eecf007162b0cabb318ab5864f6
Yuji Takaesu <taka.esu.user@gmail.com>
2020-10-04 18:20:38 +0900

    feat: Answer Question B of ABC179 by golang

--- ABC179/golang/main.go
+++ ABC179/golang/main.go
@@ -3,7 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	a()
+	//a()
+	b()
+}
+
+func b() {
+	zCnt := 0
+	var n int
+	fmt.Scan(&n)
+
+	for i := 0; i < n; i++ {
+		var d1, d2 int
+		fmt.Scanf("%d %d", &d1, &d2)
+
+		if d1 == d2 {
+			zCnt++
+			if zCnt == 3 {
+				fmt.Println("Yes")
+				return
+			}
+		} else {
+			zCnt = 0
+		}
+	}
+	fmt.Println("No")
 }
 
 func a() {
