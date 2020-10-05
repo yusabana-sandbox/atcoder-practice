f69eaf9b5337734c58ea7ac5c2d0e37d3d947455
Yuji Takaesu <taka.esu.user@gmail.com>
2020-10-05 23:38:04 +0900

    feat: Answer Question B of ABC176 by golang

--- ABC176/golang/main.go
+++ ABC176/golang/main.go
@@ -3,10 +3,34 @@ package main
 import (
 	"fmt"
 	"math"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	a()
+	//a()
+	b()
+}
+
+func b() {
+	var input string
+	var sum int = 0
+	var nums []string
+
+	fmt.Scan(&input)
+
+	nums = strings.Split(input, "")
+
+	for _, n := range nums {
+		num, _ := strconv.Atoi(n)
+		sum += num
+	}
+
+	if sum%9 == 0 {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
 }
 
 func a() {
